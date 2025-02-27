e62a13d09e5c3d462af97741725226706f891e73
Sourav khan <62439589+souravkhan12@users.noreply.github.com>
2025-02-27 14:01:39 +0530

    Update skeleton.cpp

--- vim/.config/nvim/templates/skeleton.cpp
+++ vim/.config/nvim/templates/skeleton.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using ld = long double; 
+#define int long long
+#define vii vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
 #define tcU template<class T
 #define tcUU template<typename T1, typename T2>
-tcU> using V = vector<T>; 
 #define FOR(i,a,b) for(int i=a;i<b;++i)
 #define ROF(i,a,b) for(int i=b;i>=a;i--)
 #define sz(a) (int)(a.size())
@@ -13,9 +14,11 @@ tcU> using V = vector<T>;
 #define TC(t); int t; cin >> t; while (t--)
 #define GC(i) cout << "Case #" << i << ": ";
 #define precision(n) cout << fixed << setprecision(n)
+#define nline '\n'
 void setIO(string s) { freopen((s + ".in").c_str(), "r", stdin); freopen((s + ".out").c_str(), "w", stdout); }
 const int mod = (int)1e9 + 7;
 
+tcU> using V = vector<T>; 
 tcU> T max(T &a,T b) { return a > b ? a : b; }
 tcU> T min(T &a, T b) { return a > b ? b : a; }
 tcU>istream &operator>>(istream &istream, vector<T> &v) { for (auto &it : v) { cin >> it; } return istream; }
@@ -24,10 +27,16 @@ tcUU istream &operator>>(istream &istream, pair<T1, T2> &p) { return (istream >>
 tcUU ostream &operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
 
 
-int main() {
+void solve(){
+
+}
+
+signed main() {
 	ios_base::sync_with_stdio(false),cin.tie(nullptr);
 
-	
+    TC(t){
+        solve();
+    }
 	return (0^0);
 }
 
