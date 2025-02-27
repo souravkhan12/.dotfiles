7a7bc4c0abfba1e46d034c7236d17dd97e3217a7
Sourav khan <62439589+souravkhan12@users.noreply.github.com>
2025-02-27 14:00:15 +0530

    Update keymaps.lua

--- vim/.config/nvim/lua/user/keymaps.lua
+++ vim/.config/nvim/lua/user/keymaps.lua
@@ -2,6 +2,17 @@ local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
 
+-- for checking file is js or cpp
+-- local function cpp_js(filename)
+--     local f = io.open(filename,'r')
+--     if f then
+--         f:close()
+--         return true 
+--     else 
+--         return false 
+--     end 
+-- end
+
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -30,7 +41,6 @@ keymap("n", "<leader>d", ":26<CR>dd<CR>:19", opts)
 
 --To Yank all--
 keymap("n", "gA", ":%y+<cr>", opts)
-keymap("n","<C-D>",":vertical terminal ./a.out", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
@@ -62,5 +72,7 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- js or cpp 
 keymap('', '<C-b>', ':vsplit |:te ./a.out<CR>', { noremap = true })
 keymap('n', 'gb', ':w<CR>:!g++ -g -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion % <CR>', { noremap = true })
